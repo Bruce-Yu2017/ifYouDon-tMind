284b93c89a0b15c1b076ab5485ad8a5ef9d523e7
Jiahao Yu <ilywanzi@gmail.com>
2017-08-21 14:11:31 -0700

    second

--- JiahaoYu/canihavetime.js
+++ JiahaoYu/canihavetime.js
@@ -1,36 +1,39 @@
 function asktime(hour, minute, period) {
     if (period == "AM") {
         if (minute > 30 && hour == 11) {
-                console.log("It's almost 12 in the afternoon.");
-            } 
-        else if (minute > 30 && hour < 11) {
-                console.log("It's almost " + (hour + 1) + " in the morning.");
-            } 
-        else {
-                console.log("It's just after " + hour + " in the morning.")
-            }
-        } 
+            console.log("It's almost 12 in the afternoon.");
+        } else if (minute > 30 && hour < 11) {
+            console.log("It's almost " + (hour + 1) + " in the morning.");
+        } else if (minute < 30 && hour < 11) {
+            console.log("It's just after " + hour + " in the morning.")
+        }
+    } 
     else if (period == "PM" && hour > 12 && hour < 19) {
-        if (minute > 30) {
+        if (minute > 30 && hour > 12 && hour < 19) {
             console.log("It's almost " + (hour + 1) + " in the afternoon.");
-                } 
-        else if (minute < 30) {
+        }
+        else if(minute > 30 && hour == 6) {
+            console.log("It's almost "  + 7 + " in the afternoon.");
+        }
+        else if(minute < 30) {
             console.log("It's just after " + hour + " in the afternoon.");
-        } 
-    else if (period == 'PM' && hour >= 19 && hour < 24) {
+        }
+    }
+    else if (period == "PM" && hour >= 19 && hour < 24) {
         if (minute > 30 && hour == 23) {
             console.log("It's almost 0 in the morning.");
-                } 
-        else if(minute > 30 && hour < 23){
+        }
+        else if (minute > 30 && hour < 23) {
             console.log("It's almost " + (hour + 1) + " in the evening.");
-                        } 
-        else {
-            console.log("It's just after " + hour + " in the evening.")
-                        }
-                    }
-                }
-            }
-asktime(22,34,"PM");
-        
-    
+        }
+        else if (minute < 30) {
+            console.log("It's just after " + hour + " in the evening.");
+        }
+    }
+}
 
+        
+        
+        
+        
+        
