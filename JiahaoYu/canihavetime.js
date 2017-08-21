bccd3c7e8895b02d1c35d13ee53e9a5806160329
Jiahao Yu <ilywanzi@gmail.com>
2017-08-21 13:34:08 -0700

    first

---
+++ JiahaoYu/canihavetime.js
@@ -0,0 +1,36 @@
+function asktime(hour, minute, period) {
+    if (period == "AM") {
+        if (minute > 30 && hour == 11) {
+                console.log("It's almost 12 in the afternoon.");
+            } 
+        else if (minute > 30 && hour < 11) {
+                console.log("It's almost " + (hour + 1) + " in the morning.");
+            } 
+        else {
+                console.log("It's just after " + hour + " in the morning.")
+            }
+        } 
+    else if (period == "PM" && hour > 12 && hour < 19) {
+        if (minute > 30) {
+            console.log("It's almost " + (hour + 1) + " in the afternoon.");
+                } 
+        else if (minute < 30) {
+            console.log("It's just after " + hour + " in the afternoon.");
+        } 
+    else if (period == 'PM' && hour >= 19 && hour < 24) {
+        if (minute > 30 && hour == 23) {
+            console.log("It's almost 0 in the morning.");
+                } 
+        else if(minute > 30 && hour < 23){
+            console.log("It's almost " + (hour + 1) + " in the evening.");
+                        } 
+        else {
+            console.log("It's just after " + hour + " in the evening.")
+                        }
+                    }
+                }
+            }
+asktime(22,34,"PM");
+        
+    
+
